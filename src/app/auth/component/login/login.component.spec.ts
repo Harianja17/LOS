3998842c4d1d6aa6179db25af739b1f911b0ffab
Akhlaqul Harianja <akhlaqulharianja@gmail.com>
2022-10-20 11:25:44 +0700

    for test

--- src/app/auth/component/login/login.component.spec.ts
+++ src/app/auth/component/login/login.component.spec.ts
@@ -26,20 +26,6 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  // it('successful login should return Login Token', () => {
-  //   const sampleToken: any = { token: 'dummyToken' };
-  //   let res: CommonResponse<any> = {
-  //     message: 'login success',
-  //     data: { nik: "00122300" },
-  //     code: 0,
-  //     status: ''
-  //   };
-  //   authService.login.and.returnValue(of(res));
-  //   component.loginForm.setValue({ identifier: 'dummy', password: 'dummy' });
-  //   component.login();
+  
 
-  //   expect(authService.login.calls.count()).toBe(1);
-  //   expect(sessionStorage.getItem('user')).toBeTruthy;
-
-  // });
 });
